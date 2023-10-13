5e748232e95b8218972b0ef010fa80edc8687151
Martin Akselsen <martin.akselsen@gmail.student.nhh.no>
2023-10-13 14:03:06 +0200

    task 4a

--- functions/data_transformations.r
+++ functions/data_transformations.r
@@ -1,3 +1,9 @@
+#
+# Functions
+#
+
+
+
 # Function that transforms stations_metadata into a better format
 
 transform_metadata_to_df <- function(stations_metadata) {
@@ -17,8 +23,61 @@ transform_metadata_to_df <- function(stations_metadata) {
   return(df)
 }
 
-stations_metadata_df <- 
-  stations_metadata %>% 
-  transform_metadata_to_df(.)
 
-stations_metadata_df
+# Time-function
+
+to_iso8601 <- 
+function(datetime_var, offset_in_days){
+ adjusted_datetime <- datetime_var + days(offset_in_days)
+ iso8601_string <- format(with_tz(adjusted_datetime, "UTC"), 
+                          "%Y-%m-%dT%H:%M:%SZ")
+ return(iso8601_string)
+}
+
+
+# GQL-function
+
+vol_qry <- function(id, from, to) {
+  query <- sprintf(
+    '{
+      trafficRegistrationPoint(id: "%s") {
+        id
+        name
+        latestData {
+          volumeByHour(from: "%s", to: "%s") {
+            edges {
+              node {
+                from
+                to
+                volume {
+                  car
+                  heavyVehicle
+                  total
+                }
+              }
+            }
+          }
+        }
+        location {
+          coordinates {
+            latLon {
+              lat
+              lon
+            }
+          }
+        }
+      }
+    }', id, from, to)
+  
+  return(query)
+}
+
+GQL(
+  vol_qry(
+    id=stations_metadata_df$id[1], 
+    from=to_iso8601(stations_metadata_df$latestData[1],-4),
+    to=to_iso8601(stations_metadata_df$latestData[1],0)
+  ),
+  .url = configs$vegvesen_url
+)
+
